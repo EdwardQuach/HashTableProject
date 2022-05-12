162aec32087042d55a08efa2d1ad13392280c32c
Edward <vkspecialed@gmail.com>
2022-05-12 13:43:25 -0700

    implemenatation solved

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -9,8 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
@@ -30,22 +31,66 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+    const index = this.hashMod(key);
+
+    let current = this.data[index];
+
+    while(current && current.key !== key) current = current.next;
+
+    if (current) current.value = value;
+    else {
+      const newPair = new KeyValuePair(key, value)
+      if (this.data[index]) {
+        newPair.next = this.data[index];
+        this.data[index] = newPair;
+      } else {
+        this.data[index] = newPair;
+      }
+      this.count++;
+    }
   }
 
 
   read(key) {
-    // Your code here
+    const index = this.hashMod(key);
+    let current = this.data[index];
+    while(current) {
+      if(current.key===key){
+        return current.value;
+      }
+      current=current.next;
+    }
+    return undefined;
   }
 
 
   resize() {
-    // Your code here
+    this.capacity *= 2;
+    let arr = this.data;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
+    for(let ele of arr){
+      let current = ele.next;
+      while(current){
+        this.insert(current.key, current.value);
+        current=current.next;
+      }
+      this.insert(ele.key, ele.value);
+    } 
   }
 
 
   delete(key) {
-    // Your code here
+    const index = this.hashMod(key);
+    let current = this.data[index];
+    while(current) {
+      if(current.key === key){  
+        current.value = undefined;
+        this.count--;
+      }
+      current = current.next;
+    }
+    return `Key not found`
   }
 }
 
